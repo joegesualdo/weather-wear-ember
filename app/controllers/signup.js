dd3806098cae948e8c50afd11ecfc9ac495a607d
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-30 19:16:16 -0400

    Add a success and failure callback when creating a user

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -7,8 +7,17 @@ export default Ember.Controller.extend({
         email: this.get('email'),
         password: this.get('password')
       });
-      user.save();
-      this.transitionToRoute('index');
+
+      // Saves the user and handles the success and error responses
+      var self = this;
+      user.save().then(function() {
+          self.transitionToRoute('index');
+        }, function(resp) {
+          if (resp.responseJSON) {
+            // self.get('model').set('errors', resp.responseJSON.errors);
+            console.log('errror', resp.responseJSON.message);
+          } 
+      });
     }
   }
 });
